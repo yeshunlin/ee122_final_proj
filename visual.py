91e2a81db7ac924b4660864d88274adbe04f2371
Maxwell Gerber <mgerber@berkeley.edu>
2017-04-13 20:55:14 -0700

    streaming gif generation instead of holding all in mem

--- visual.py
+++ visual.py
@@ -1,8 +1,11 @@
 from src.discrete import Environment
 from src.components import Car, StreetGraph, EuclideanNode
-import matplotlib.pyplot as plt
 import math
-import imageio 
+import matplotlib
+matplotlib.use('TKAgg')
+from matplotlib import pyplot as plt, animation
+import imageio
+import os
 
 
 """Vehicle Simulations"""
@@ -45,19 +48,22 @@ def drawNodes(i, xlist, ylist, r):
 
 
 #Testing Car Unit 
-g = StreetGraph()
-g.add_node(3, 0, 'A')
-g.add_node(2, 12, 'B')
-g.add_edge('A', 'B', 10)
-ferrari = g.add_car('A', 'B', 'One fast car', 0.5)
-f2 = g.add_car('A', 'B', 'blah2', 1)
+g = StreetGraph(nodeCls = EuclideanNode)
+g.add_node(1, 1, 'A')
+g.add_node(11, 1, 'B')
+g.add_node(11, 11, 'C')
+g.add_node(1, 11, 'D')
+g.add_edge('A', 'B')
+g.add_edge('C', 'B')
+g.add_edge('D', 'C')
+g.add_edge('A', 'D')
+ferrari = g.add_car('A', 'D', 'One fast car', 0.1)
+f2 = g.add_car('C', 'A', 'blah2', .17)
 
-numIter = 6 #number of driving iterations
+numIter = 200 #number of driving iterations
 
 images = []
-
-for i in range(numIter):
-  plt.figure(i)
+with imageio.get_writer("movie.gif", 'GIF', mode='I', duration=.1,) as writer:
   GRAPHSIZE = 15
   ticks = range(0, GRAPHSIZE + 1)
   plt.hold(True)
@@ -66,30 +72,30 @@ for i in range(numIter):
   plt.yticks(ticks)
   plt.xlim(0, GRAPHSIZE)
   plt.ylim(0, GRAPHSIZE)
+  for i in range(numIter):
+    print("Generating frame {}".format(i))
+    
+    xlist = []
+    ylist = []
 
-  xlist = []
-  ylist = []
+    for c in g._cars:
+        x, y = c.position()
+        xlist.append(x)
+        ylist.append(y)
+        plt.plot(x, y, 'bo')
+        drawNodes(0, xlist,ylist, 3)
+        try:
+          c.drive()
+        except:
+          pass
 
-  for c in g._cars:
-      x, y = c.position()
-      xlist.append(x)
-      ylist.append(y)
-      plt.plot(x, y, 'bo')
-      drawNodes(0, xlist,ylist, 3)
-      c.drive()
+    filename = 'fig' + str(i) + '.png'
+    plt.savefig(filename)
+    plt.clf()
 
-  filename = 'fig' + str(i) + '.png'
-  plt.savefig(filename)
-  images.append(imageio.imread(filename))
-
-exportname = "movie.gif"
-kargs = { 'duration': 2 }
-imageio.mimsave(exportname, images, 'GIF', **kargs)
-
-
- 
-
-
+    kargs = { }
+    writer.append_data(imageio.imread(filename), **kargs)
+    os.remove(filename)
 
 
 
